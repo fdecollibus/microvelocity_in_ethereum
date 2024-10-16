27a3d94948da1c83a5f356ed2bfc7e205a75dd6d
Francesco deCollibus <francesco.decollibus@business.uzh.ch>
2024-10-16 23:08:00 +0200

    fixed slicer

--- 2_velocity_slicer.py
+++ 2_velocity_slicer.py
@@ -6,7 +6,7 @@ import os
 #Create a temp folder if not exists
 if not os.path.exists('temp'):
     os.makedirs('temp')
-with open('int_raw_accounts.pickle', 'rb') as f:
+with open('temp/int_raw_accounts.pickle', 'rb') as f:
     accounts = pickle.load(f)
 filecounter=1
 for _i in range(0,len(accounts.keys()),20000):
